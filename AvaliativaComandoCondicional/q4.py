105f23a4fb88b5986b5b581ff3bb71088730bfca
Vinief <viniaedu314@gmail.com>
2025-05-25 12:40:20 -0300

    Update q4.py
    mais comentarios

--- AvaliativaComandoCondicional/q4.py
+++ AvaliativaComandoCondicional/q4.py
@@ -1,15 +1,17 @@
-print ('esse calcula o tempo de um mês para o outro no mesmo ano')
+print ('esse calcula o tempo de um mês para o outro no mesmo ano (considerando que não é ano bissexto)')
 di = int(input('diga o dia inicial:')) 
 mi = int(input('diga o mês inicial:'))
 df = int(input('diga o dia final:'))
 mf = int(input('diga o mês final:'))
-
+#caso os meses sejam maiores que 12 ou menores que 0
 if mi > 12 or mf > 12 or mi < 0 or mf < 0:
     print('essa data não existe!!!')
+    #caso os meses que tem 30 dias tenham mais que 30 dias
     if (mi == 4 or mi == 7 or mi == 9 or mi == 11) and di > 30:
         print('essa data não existe!!!') 
         if (mf == 4 or mf == 7 or mf == 9 or mf == 11) and df > 30:
             print('essa data não existe!!!')
+            #caso o usuário tente usar ano bissexto  
             if mi == 2 and di >= 29:
                 print('não é ano bissexto!!!')
             if mf == 2 and df >= 29:
